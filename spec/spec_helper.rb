e52b28c722b62be14300ca65ac836abf15d23d60
brainopia <brainopia@evilmartians.com>
2013-02-23 01:52:02 +0400

    Create testing keyspace if needed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,12 @@
 require 'cassandra/mapper'
+
+TEST_KEYSPACE = 'test_mapper'
+
+# create testing keyspace if needed
+cassandra = Cassandra.new('system')
+unless cassandra.keyspaces.include? TEST_KEYSPACE
+  cassandra.add_keyspace Cassandra::Keyspace.new \
+    name: TEST_KEYSPACE,
+    strategy_options: { 'replication_factor' => '1' },
+    cf_defs: []
+end
