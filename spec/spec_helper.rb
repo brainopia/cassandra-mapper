f26c253480973261f7506f705078be7d89c1fada
brainopia <brainopia@evilmartians.com>
2013-02-23 01:56:02 +0400

    Add missing declaration to create testing keyspace

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,7 @@ cassandra = Cassandra.new('system')
 unless cassandra.keyspaces.include? TEST_KEYSPACE
   cassandra.add_keyspace Cassandra::Keyspace.new \
     name: TEST_KEYSPACE,
+    strategy_class: 'SimpleStrategy',
     strategy_options: { 'replication_factor' => '1' },
     cf_defs: []
 end
