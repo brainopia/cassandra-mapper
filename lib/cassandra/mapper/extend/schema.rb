a6c3659b778634ca2d07835f974c658bb9cef957
brainopia <brainopia@evilmartians.com>
2013-06-24 08:57:10 +0400

    Return keyspaces for appropriate server

--- lib/cassandra/mapper/extend/schema.rb
+++ lib/cassandra/mapper/extend/schema.rb
@@ -17,7 +17,7 @@ class Cassandra::Mapper
 
   def self.keyspaces
     schema[:keyspaces].map do |name|
-      Cassandra.new "#{name}_#{env}"
+      Cassandra.new "#{name}_#{env}", server
     end
   end
 
